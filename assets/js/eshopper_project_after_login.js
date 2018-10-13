2e89152ee935bd1e043aa951262761a4b37e7337
pinank3 <37895606+pinank3@users.noreply.github.com>
2018-10-13 17:02:01 +0530

    Add files via upload

---
+++ assets/js/eshopper_project_after_login.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+	$(".btn-update").click(function(){
+		records =$("#update_form").serialize();
+		$.post("../controllers/password-action.php",records).success(function(res){
+			$(".msg_update").html(res);
+		})
+	})
+	$(".btn-brand").click(function(){
+		// alert(1)
+		records =$("#brand_form").serialize();
+		$.post("../controllers/brand-action.php",records).success(function(res){
+			$(".msg_brand").html(res);
+		})
+	})
+	$(".btn-category").click(function(){
+		// alert(1)
+		records =$("#category_form").serialize();
+		$.post("../controllers/category-action.php",records).success(function(res){
+			$(".msg_category").html(res);
+		})
+	})
+	$(".btn-product").click(function(){
+		// alert(111)
+		// record=$("#product_form").serialize();
+		formobj=document.getElementById("product_form");
+		// alert(formobj)
+		dataobj= new FormData(formobj);
+		// alert(dataobj)
+		$.ajax({
+			data:dataobj,
+			url:"../controllers/product-action.php",
+			contentType:false,
+			processData:false,
+			type:"post",
+			success:function(res){
+				$(".msg_product").html(res);
+			},
+			error :function(errmsg){
+				console.log(errmsg);
+			}
+			});
+	})
+
+})
